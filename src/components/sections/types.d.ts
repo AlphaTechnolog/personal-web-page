9f6fd2c1687396d3a4dabc39a75eb9ee3a004627
Gabriel Guerra <gfranklings@gmail.com>
2024-10-28 10:26:07 -0400

    feat: Projects section foundation

---
+++ src/components/sections/types.d.ts
@@ -0,0 +1,19 @@
+export enum Scheme {
+  dark,
+  light,
+}
+
+export interface Technology {
+  color: string;
+  name: string;
+  scheme?: Scheme;
+}
+
+export interface Project {
+  src: typeof import("*.png"),
+  name: string;
+  simpleDescription: string;
+  technologies: Technology[];
+  description: string;
+  code: string;
+}
