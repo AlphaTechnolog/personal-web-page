1c4d2234ac59014d7eac2002fa06a7834adb9fbe
Gabriel Guerra <gfranklings@gmail.com>
2024-10-29 05:45:23 -0400

    chore: Adding some technologies to about me

---
+++ src/data/technologies.ts
@@ -0,0 +1,64 @@
+import { type Technology, Scheme } from "../components/sections/types.d";
+
+export const c: Technology = {
+  name: "C",
+  color: "#6099C9",
+}
+
+export const javascript: Technology = {
+  name: "JavaScript",
+  color: "#F7E854"
+}
+
+export const vue: Technology = {
+  name: "Vue",
+  color: "#42B782",
+}
+
+export const zig: Technology = {
+  name: "Zig",
+  color: "#EAA217",
+}
+
+export const rust: Technology = {
+  name: "Rust",
+  color: "#ef4a00",
+}
+
+export const nextjs: Technology = {
+  name: "Next.JS",
+  color: "#000000",
+  scheme: Scheme.dark,
+}
+
+export const react: Technology = {
+  name: "React.JS",
+  color: "#79d8f7",
+}
+
+export const nuxtjs: Technology = {
+  name: "Nuxt.JS",
+  color: "#02dd83",
+}
+
+export const django: Technology = {
+  name: "Django",
+  color: "#092d1f",
+  scheme: Scheme.dark,
+}
+
+export const python: Technology = {
+  name: "Python",
+  color: "#3772a0",
+}
+
+export const gtk: Technology = {
+  name: "Gtk",
+  color: "#000000",
+  scheme: Scheme.dark,
+}
+
+export const tailwind: Technology = {
+  name: "Tailwind CSS",
+  color: "#0D97D6"
+}
